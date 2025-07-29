adeb568710aa7da514c37a064a00b2ce7a258c49
RainCreatorHub <rain.hub.creator@gmail.com>
2025-07-29 08:57:20 -0300

    Update main.lua

--- main.lua
+++ main.lua
@@ -3,14 +3,3 @@ urls.Owner = "https://raw.githubusercontent.com/RainCreatorHub/"
 urls.Repository = urls.Owner .. "MoonLibTest/refs/heads/main/"
 urls.Elements = urls.Repository .. "Elementos/"
 urls.Window = urls.Repository .. "Window/"
-
--- Carregar módulos
-local OrionLibV2 = require(urls.Window .. "MakeWindow.lua")
-local Tabs = require(urls.Window .. "MakeTab.lua")
-
--- Carregar elementos
-local AddSection = require(urls.Elements .. "AddSection.lua")
-local AddLabel = require(urls.Elements .. "AddLabel.lua")
-local AddButton = require(urls.Elements .. "AddButton.lua")
-local AddToggle = require(urls.Elements .. "AddToggle.lua")
-local AddDropdown = require(urls.Elements .. "AddDropdown.lua")
